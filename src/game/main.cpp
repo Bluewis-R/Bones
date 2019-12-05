c7e0159cf5d0665113b12fbb70554794ecf6512b
Lewis Remington <lewispremington@gmail.com>
2019-12-05 01:05:15 +0000

    added commented OpenAL code in main

--- src/game/main.cpp
+++ src/game/main.cpp
@@ -2,10 +2,21 @@
 #include <memory>
 
 #include <Bones/Bones.h>
-//#include "OpenGL/triRenderer.h"
+
+#include <AL/al.h>
+#include <AL/alc.h>
+#include "Bones/stb_vorbis.h"
 
 // TODO: Put into Bones.h
 #include "Bones/Macros.h"
+
+struct SM
+{
+public:
+  ALCdevice* device;
+  ALCcontext* context;
+};
+
 
 int main()
 {
@@ -14,10 +25,40 @@ int main()
   std::shared_ptr<GameObject> triangle = core->AddGameObject();
   std::shared_ptr<MeshRenderer> mr = triangle->AddComponents<MeshRenderer>();
 
-  std::shared_ptr<BMesh> mesh = core->GetResources()->Load<BMesh>("model/curuthers");
+  std::shared_ptr<BMesh> mesh = core->GetResources()->Load<BMesh>("../model/curuthers");
   mr->SetMesh(mesh);
+  /*
+  ALCdevice* device;
+  ALCcontext* context;
 
-  
+  SM* sm = new SM();
+
+  sm->device = alcOpenDevice(NULL);
+  if (!device)
+  {
+    throw std::exception();
+  }
+
+  sm->context = alcCreateContext(device, NULL);
+  if (!context)
+  {
+    alcCloseDevice(device);
+    throw std::exception();
+  }
+  if (!alcMakeContextCurrent(context))
+  {
+    alcDestroyContext(context);
+    alcCloseDevice(device);
+    throw std::exception();
+  }
+
+  //Destroy
+  //alcMakeContextCurrent(NULL);
+  //alcDestroyContext(context);
+  */
+
+
+
   core->Start();
 
 
